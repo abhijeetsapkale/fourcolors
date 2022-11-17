feb31fbdc2c63423c57bd8e213f35bd0b6f4d9cf
monika-octet <monika@octet.design>
2022-11-17 14:42:47 +0530

    resize

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import 'antd/dist/antd.css';
 import '../styles/globals.css';
 import '../styles/main.css';
+import NoSSRWrapper from "../components/no-ssr-wrapper";
 function MyApp({ Component, pageProps }) {
   return (
+    <NoSSRWrapper>
     <Component {...pageProps} />
+    </NoSSRWrapper>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,41 @@ import Sidebar from '../components/sidebar';
 import SearchHeader from '../components/search';
 import Link from 'next/link';
 import { Table, Button, Dropdown, Menu, Image,  Space} from 'antd';
+// import ResizableAntdTable from 'resizable-antd-table';
+// import NoSSRWrapper from "../components/no-ssr-wrapper";
 
-
-
+import 'antd/dist/antd.css';
+import React, { useState } from 'react';
+import { Resizable } from 'react-resizable';
+const ResizableTitle = (props) => {
+  const { onResize, width, ...restProps } = props;
+  if (!width) {
+    return <th {...restProps} />;
+  }
+  return (
+    <Resizable
+      width={width}
+      height={0}
+      handle={
+        <span
+          className="react-resizable-handle"
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+        />
+      }
+      onResize={onResize}
+      draggableOpts={{
+        enableUserSelectHack: false,
+      }}
+    >
+      <th {...restProps} />
+    </Resizable>
+  );
+};
 export default function AllDocuments({  }) {
-    const columns = [
-        {
+  const [columns, setColumns] = useState([
+   {
             title: 'JOB DETAILS',
             dataIndex: 'jobdetail',
             key: 'jobdetail',
@@ -18,7 +47,7 @@ export default function AllDocuments({  }) {
                     <p>{jobdetail.date}</p>
                 </>
             },
-            width: '25%'
+            width: 250
         },
         {
             title: 'Status',
@@ -44,7 +73,7 @@ export default function AllDocuments({  }) {
                     return <span className='status light-green'>{' '}{status}</span>
                 }
             }, 
-            width: '25%',
+            width: 250,
             align:'center'
         },
         {
@@ -54,7 +83,7 @@ export default function AllDocuments({  }) {
             render: receiver => {
                 return receiver.map((rc) =>  <p className='text-dark fw-500' key={rc.id}>{rc}</p>)
             },
-            width: '20%'
+            width: 200
         },
         {
             title: 'INITIATOR / COMPANY',
@@ -63,7 +92,7 @@ export default function AllDocuments({  }) {
             render: initiator => {
                 return <p className='text-dark fw-500'>{initiator}</p>
             },
-            width: '20%'
+            width:200
         },
         {
             title: 'ACTION',
@@ -81,149 +110,181 @@ export default function AllDocuments({  }) {
                     </Space>
                 </>
             },
-            width: '10%'
+            width: 100
         },
-    ];
-
-    const dataSource = [
+  ]);
+ 
+const dataSource = [
+    {
+        key: '1',
+        jobdetail: {
+            jobId: 'Job0000024',
+            mainTitle: 'TC Job Document Item',
+            date: '11 Apr, 2022; 05:57'
+        },
+        status: 'Processing',
+        receiver: ['Wolfix Cai /  Four Colors', 'Ashutosh Palekar /  FourColors Ply'],
+        initiator: 'User DX / External Co. 1',
+        action:''
+    },
+    {
+        key: '2',
+        jobdetail: {
+            jobId: 'Job0000024',
+            mainTitle: 'TC Job Document Item',
+            date: '11 Apr, 2022; 05:57'
+        },
+        status: 'Draft',
+        receiver: ['Wolfix Cai /  Four Colors', 'Ashutosh Palekar /  FourColors Ply'],
+        initiator: 'User DX / External Co. 1',
+        action:''
+    },
+    {
+        key: '3',
+        jobdetail: {
+            jobId: 'Job0000024',
+            mainTitle: 'TC Job Document Item',
+            date: '11 Apr, 2022; 05:57'
+        },
+        status: 'OFTP Ready',
+        receiver: ['Wolfix Cai /  Four Colors', 'Ashutosh Palekar /  FourColors Ply'],
+        initiator: 'User DX / External Co. 1',
+        action:''
+    },
+    {
+        key: '4',
+        jobdetail: {
+            jobId: 'Job0000024',
+            mainTitle: 'TC Job Document Item',
+            date: '11 Apr, 2022; 05:57'
+        },
+        status: 'Cancelled',
+        receiver: ['Wolfix Cai /  Four Colors', 'Ashutosh Palekar /  FourColors Ply'],
+        initiator: 'User DX / External Co. 1',
+        action:''
+    },
+    {
+        key: '5',
+        jobdetail: {
+            jobId: 'Job0000024',
+            mainTitle: 'TC Job Document Item',
+            date: '11 Apr, 2022; 05:57'
+        },
+        status: 'Sent',
+        receiver: ['Wolfix Cai /  Four Colors', 'Ashutosh Palekar /  FourColors Ply'],
+        initiator: 'User DX / External Co. 1',
+        action:''
+    },
+    {
+        key: '6',
+        jobdetail: {
+            jobId: 'Job0000024',
+            mainTitle: 'TC Job Document Item',
+            date: '11 Apr, 2022; 05:57'
+        },
+        status: 'Received',
+        receiver: ['Wolfix Cai /  Four Colors', 'Ashutosh Palekar /  FourColors Ply'],
+        initiator: 'User DX / External Co. 1',
+        action:''
+    }
+];
+const menu = (
+    <Menu
+      items={[
         {
-            key: '1',
-            jobdetail: {
-                jobId: 'Job0000024',
-                mainTitle: 'TC Job Document Item',
-                date: '11 Apr, 2022; 05:57'
-            },
-            status: 'Processing',
-            receiver: ['Wolfix Cai /  Four Colors', 'Ashutosh Palekar /  FourColors Ply'],
-            initiator: 'User DX / External Co. 1',
-            action:''
+          key: '1',
+          label: (
+            <Link href="/">
+                <a>
+                    1st menu item
+                </a>
+            </Link>
+          ),
         },
         {
-            key: '2',
-            jobdetail: {
-                jobId: 'Job0000024',
-                mainTitle: 'TC Job Document Item',
-                date: '11 Apr, 2022; 05:57'
-            },
-            status: 'Draft',
-            receiver: ['Wolfix Cai /  Four Colors', 'Ashutosh Palekar /  FourColors Ply'],
-            initiator: 'User DX / External Co. 1',
-            action:''
+          key: '2',
+          label: (
+            <Link href="/">
+                <a>
+                    2nd menu item
+                </a>
+            </Link>
+          )
         },
         {
-            key: '3',
-            jobdetail: {
-                jobId: 'Job0000024',
-                mainTitle: 'TC Job Document Item',
-                date: '11 Apr, 2022; 05:57'
-            },
-            status: 'OFTP Ready',
-            receiver: ['Wolfix Cai /  Four Colors', 'Ashutosh Palekar /  FourColors Ply'],
-            initiator: 'User DX / External Co. 1',
-            action:''
-        },
-        {
-            key: '4',
-            jobdetail: {
-                jobId: 'Job0000024',
-                mainTitle: 'TC Job Document Item',
-                date: '11 Apr, 2022; 05:57'
-            },
-            status: 'Cancelled',
-            receiver: ['Wolfix Cai /  Four Colors', 'Ashutosh Palekar /  FourColors Ply'],
-            initiator: 'User DX / External Co. 1',
-            action:''
-        },
-        {
-            key: '5',
-            jobdetail: {
-                jobId: 'Job0000024',
-                mainTitle: 'TC Job Document Item',
-                date: '11 Apr, 2022; 05:57'
-            },
-            status: 'Sent',
-            receiver: ['Wolfix Cai /  Four Colors', 'Ashutosh Palekar /  FourColors Ply'],
-            initiator: 'User DX / External Co. 1',
-            action:''
-        },
-        {
-            key: '6',
-            jobdetail: {
-                jobId: 'Job0000024',
-                mainTitle: 'TC Job Document Item',
-                date: '11 Apr, 2022; 05:57'
-            },
-            status: 'Received',
-            receiver: ['Wolfix Cai /  Four Colors', 'Ashutosh Palekar /  FourColors Ply'],
-            initiator: 'User DX / External Co. 1',
-            action:''
+          key: '3',
+          label: (
+            <Link href="/">
+                <a>
+                    3rd menu item
+                </a>
+            </Link>
+          )
         }
-    ];
+      ]}
+    />
+);
+  const handleResize =
+    (index) =>
+    (_, { size }) => {
+      const newColumns = [...columns];
+      newColumns[index] = {
+        ...newColumns[index],
+        width: size.width,
+      };
+      setColumns(newColumns);
+    };
+  const mergeColumns = columns.map((col, index) => ({
+    ...col,
+    onHeaderCell: (column) => ({
+      width: column.width,
+      onResize: handleResize(index),
+    }),
+  }));
+  return (
+    <>
+     <div className='main-layout'>
+                    <Sidebar></Sidebar>
+                    <div>
+                        <header className='header'>
+                            <h1>All Jobs</h1>
+                            <SearchHeader></SearchHeader>
+                            <Space className="header-right">
+                                <Button>
+                                    <Space align="center">
+                                        <Image src="/images/export.svg" alt="export" height={11} width={10} preview={false} className="img-export"/> 
+                                        <span>Export</span>
+                                    </Space>
+                                </Button>
+                                <Button type='primary'>New Job</Button>
+                            </Space>
+                        </header>
+                        <main className='main-pad'>
+                            {/* <Table dataSource={dataSource} columns={columns}  pagination= { 
+                                {pageSizeOptions: ['2', '4', '6'], showSizeChanger: true, }
+                            }  className="table-1"/> */}
+                               
+                               <Table
+      
+      components={{
+        header: {
+          cell: ResizableTitle,
+        },
+      }}
+      columns={mergeColumns}
+      dataSource={dataSource}
+      className="table-1"
+    />
+                                {/* <ResizableAntdTable columns={columns} dataSource={dataSource}  pagination= { 
+                                    {pageSizeOptions: ['2', '4', '6'], showSizeChanger: true, }
+                                }  className="table-1"/> */}
     
-    const menu = (
-        <Menu
-          items={[
-            {
-              key: '1',
-              label: (
-                <Link href="/">
-                    <a>
-                        1st menu item
-                    </a>
-                </Link>
-              ),
-            },
-            {
-              key: '2',
-              label: (
-                <Link href="/">
-                    <a>
-                        2nd menu item
-                    </a>
-                </Link>
-              )
-            },
-            {
-              key: '3',
-              label: (
-                <Link href="/">
-                    <a>
-                        3rd menu item
-                    </a>
-                </Link>
-              )
-            }
-          ]}
-        />
-    );
-        
-
-    return (
-        <>
-            <div className='main-layout'>
-                <Sidebar></Sidebar>
-                <div>
-                    <header className='header'>
-                        <h1>All Jobs</h1>
-                        <SearchHeader></SearchHeader>
-                        <Space className="header-right">
-                            <Button>
-                                <Space align="center">
-                                    <Image src="/images/export.svg" alt="export" height={11} width={10} preview={false} className="img-export"/> 
-                                    <span>Export</span>
-                                </Space>
-                            </Button>
-                            <Button type='primary'>New Job</Button>
-                        </Space>
-                    </header>
-                    <main className='main-pad'>
-                        <Table dataSource={dataSource} columns={columns}  pagination= { 
-                            {pageSizeOptions: ['2', '4', '6'], showSizeChanger: true, }
-                        }  className="table-1"/>
-                        
-                    </main>
+                            
+                        </main>
+                    </div>
                 </div>
-            </div>
         </>
-    )
-}
+    
+  );
+};
+
