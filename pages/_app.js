a47af41e479a0e504adf6050d3ffaefe84cce075
monika-octet <monika@octet.design>
2022-12-21 13:13:10 +0530

    all code

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import 'antd/dist/antd.css';
-import '../styles/common.css';
+
+import '../styles/globals.css';
 import '../styles/login.css';
 import '../styles/main.css';
+import 'antd/dist/antd.css';
 import NoSSRWrapper from "../components/no-ssr-wrapper";
 function MyApp({ Component, pageProps }) {
   return (
